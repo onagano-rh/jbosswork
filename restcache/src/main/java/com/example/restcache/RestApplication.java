7ee6138862a6d921dee5bfc2273499d2589e4f6c
Osamu Nagano <onagano@redhat.com>
2016-08-30 18:16:39 +0900

    Add a sample JDG library mode web application

---
+++ restcache/src/main/java/com/example/restcache/RestApplication.java
@@ -0,0 +1,8 @@
+package com.example.restcache;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest")
+public class RestApplication extends Application {
+}
