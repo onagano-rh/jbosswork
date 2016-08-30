217daacf1d2ae47559796f89a4f0001cd7bb3acf
Osamu Nagano <onagano@redhat.com>
2016-08-30 17:47:26 +0900

    Add a simple Hot Rod Java client for JDG server

---
+++ hotrodclient/src/main/java/com/example/App.java
@@ -0,0 +1,111 @@
+package com.example;
+
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+
+public class App {
+    private static Logger LOG = Logger.getLogger(App.class.getName());
+    private static boolean INTERACTIVE = System.console() != null;
+    private static BufferedReader STDIN = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws Exception {
+        LOG.info("Started.");
+        if (INTERACTIVE) write("Type 'Ctrl-d' to quit, 'Ctrl-u' to clear.");
+        boolean loop = true;
+        HotRodClient program = null;
+        while (loop) {
+            String input = read("hoge> ");
+            if (input == null) {
+                loop = false;
+            } else {
+                String[] cmd  = input.split("\\s");
+                try {
+                    switch (cmd[0]) {
+
+                    case "connect":
+                        if (cmd.length > 2)
+                            program = new HotRodClient(cmd[1], cmd[2]);
+                        else
+                            program = new HotRodClient(cmd[1], null);
+                        break;
+
+                    case "cache":
+                        if (cmd.length > 1)
+                            program.setCache(cmd[1]);
+                        else
+                            program.setCache(null);
+                        break;
+
+                    case "disconnect":
+                        program.disconnect();
+                        break;
+
+                    case "get":
+                        write(program.get(cmd[1]));
+                        break;
+
+                    case "getmeta":
+                        write(program.getWithMetadata(cmd[1]));
+                        break;
+
+                    case "getBulk":
+                        program.getBulk();
+                        break;
+
+                    case "put":
+                        write(program.put(cmd[1], cmd[2]));
+                        break;
+
+                    case "putIfAbsent":
+                        write(program.putIfAbsent(cmd[1], cmd[2]));
+                        break;
+
+                    case "remove":
+                        write(program.remove(cmd[1]));
+                        break;
+
+                    case "sleep":
+                        try {
+                            long usec = Long.parseLong(cmd[1]);
+                            Thread.sleep(usec);
+                        } catch (Exception ignore) {}
+                        break;
+
+                    case "size":
+                        write(program.size());
+                        break;
+
+                    case "clear":
+                        program.clear();
+                        break;
+
+                    case "clearAsync":
+                        program.clearAsync();
+                        break;
+
+                    default:
+                        break;
+                    }
+                } catch (Exception e) {
+                    write("Something wrong: " + e.getMessage());
+                    LOG.log(Level.FINE, "Something wrong", e);
+                }
+            } // end else
+        } // end while
+        LOG.info("Finished.");
+    }
+
+    private static String read(String prompt) {
+        if (INTERACTIVE) System.out.print(prompt);
+        String line = null;
+        try {
+            line = STDIN.readLine();
+        } catch (Exception ignore) {}
+        return line;
+    }
+
+    private static void write(Object mesg) {
+        System.out.println(mesg);
+    }
+}
---
+++ hotrodclient/src/main/java/com/example/HotRodClient.java
@@ -0,0 +1,96 @@
+package com.example;
+
+import java.util.logging.*;
+
+import org.infinispan.client.hotrod.*;
+import org.infinispan.client.hotrod.configuration.*;
+
+import java.util.Map;
+import org.infinispan.commons.util.concurrent.NotifyingFuture;
+
+public class HotRodClient {
+    private static Logger LOG = Logger.getLogger(HotRodClient.class.getName());
+
+    private RemoteCacheManager cacheManager;
+    private RemoteCache<String, Object> cache;
+
+    public HotRodClient(String serverList, String cacheName) {
+        connect(serverList);
+        setCache(cacheName);
+    }
+
+    private void connect(String serverList) {
+        LOG.info("connect called: " + serverList);
+        ConfigurationBuilder builder = new ConfigurationBuilder();
+        builder.addServers(serverList);
+        //builder.maxRetries(0);
+        cacheManager = new RemoteCacheManager(builder.build());
+        LOG.info("Connected.");
+    }
+
+    public void disconnect() {
+        LOG.info("disconnect called.");
+        if (cacheManager != null) {
+            cacheManager.stop();
+            cacheManager = null;
+            cache = null;
+            LOG.info("Disconnected");
+        }
+    }
+
+    public void setCache(String cacheName) {
+        if (cacheName != null) {
+            cache = cacheManager.getCache(cacheName);
+        } else {
+            cache = cacheManager.getCache();
+        }
+        LOG.info("Selected cache: " + cache.getName());
+    }
+
+    public Object get(String key) {
+        Object val = cache.get(key);
+        return val;
+    }
+
+    public MetadataValue<Object> getWithMetadata(String key) {
+        return cache.getWithMetadata(key);
+    }
+
+    public void getBulk() {
+        Map<String, Object> map = cache.getBulk();
+        for (String k : map.keySet()) {
+            Object v = map.get(k);
+            LOG.info("Got " + k + " : " + v);
+        }
+    }
+
+    public Object put(String key, Object value) {
+        return cache.put(key, value);
+    }
+
+    public Object putIfAbsent(String key, Object value) {
+        return cache.withFlags(Flag.FORCE_RETURN_VALUE).putIfAbsent(key, value);
+    }
+
+    public Object remove(String key) {
+        Object val = cache.remove(key);
+        return val;
+    }
+
+    public int size() {
+        return cache.size();
+    }
+
+    public void clear() {
+        cache.clear();
+    }
+
+    public void clearAsync() {
+        try {
+            cache.clearAsync().get();
+        }
+        catch (Exception e) {
+            LOG.log(Level.FINE, "Error in clearAsync", e);
+        }
+    }
+}
