97a2a676366acf641f27d36a2951c6a9ab73695c
Osamu Nagano <onagano@redhat.com>
2017-02-08 17:26:07 +0900

    Revert to JDG 7

--- standalonecache/src/main/java/com/example/standalonecache/App.java
+++ standalonecache/src/main/java/com/example/standalonecache/App.java
@@ -2,6 +2,9 @@ package com.example.standalonecache;
 
 import java.io.IOException;
 
+import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
+
 import org.infinispan.Cache;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.Configuration;
@@ -11,10 +14,14 @@ import org.infinispan.manager.EmbeddedCacheManager;
 
 public class App
 {
+    static final Logger LOG = LogManager.getLogger(App.class);
+
     int maxEntries = Integer.parseInt(System.getProperty("maxEntries", "10"));
     int numberOfPut = Integer.parseInt(System.getProperty("numberOfPut", "100"));
 
     void runTest() {
+        LOG.entry();
+        LOG.info("Start now");
 //        Configuration c = new ConfigurationBuilder()
 //            .clustering().cacheMode(CacheMode.LOCAL)
 //            .eviction().maxEntries(maxEntries)
@@ -37,6 +44,7 @@ public class App
                 System.out.printf("Loop: %d, cache size: %d\n", i, cache.size());
             }
         }
+        LOG.exit();
     }
 
     public static void main(String[] args) {
