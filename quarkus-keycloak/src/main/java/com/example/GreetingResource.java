74ae17e85591cc0f892252ea86a39bb5aa0649f8
Osamu Nagano <onagano@redhat.com>
2022-12-17 17:41:08 +0900

    Protect by Keycloak

--- quarkus-keycloak/src/main/java/com/example/GreetingResource.java
+++ quarkus-keycloak/src/main/java/com/example/GreetingResource.java
@@ -5,12 +5,34 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
     @GET
+    @Path("/hello")
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello RESTEasy";
+    }
+
+    @GET
+    @Path("/public")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String resPublic() {
+        return "public";
+    }
+
+    @GET
+    @Path("/protected")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String resProtected() {
+        return "protected";
+    }
+
+    @GET
+    @Path("/protected/admin")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String resProtectedAdmin() {
+        return "protected admin";
     }
 }
